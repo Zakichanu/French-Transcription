33a287f2d5f6a7bfeef41fbb17a2ede271f7c567
Zakaria Tobbal <tzakaria@hotmail.fr>
2022-05-18 22:46:14 +0200

    first version of the program

---
+++ readme.md
@@ -0,0 +1,18 @@
+# French Transcription script
+
+Python program to transcribe french videos into text. Made with searchrecognition library and pydub.
+
+## Installation
+
+- Import libraries :
+```python -m pip install -r lib.txt```
+
+- Used python 3.9.8 version to run my script
+
+- And make sure you have an mp4 file to run the script properly.
+
+- Run the script :
+```python transcription.py```
+
+> Made by Zakichanu
+
--- transcription.py
+++ transcription.py
@@ -8,24 +8,15 @@ def getVideoToWav(videoname: str):
     com1 = f"ffmpeg -i {videoname} video.mp3"
     com2 = "ffmpeg -i video.mp3 -f segment -segment_time 300 -c copy audio_chunks/chunk%03d.wav"
     os.system(com1)
+    os.mkdir("audio_chunks")
     os.system(com2)
-
-def wav2ytext(language="fr-FR"):
-    r = sr.Recognizer()
-    with sr.AudioFile("video.wav") as source:
-        audio=r.record(source)
-        print("Transcription: "+r.recognize_google(audio, language=language))
-
-getVideoToWav("temoignage.mp4")
-##wav2ytext("fr-FR")
 
 
 def silencebasedconversion(path: str):
     song = AudioSegment.from_wav(path)
-    fh = open("recognized.txt", "w+")
+    fh = open(os.path.dirname(__file__) + "/recognized.txt", "a+")
     print("Processing chunk "+path)
-
-    chunks = split_on_silence(song, min_silence_len = 1000, silence_thresh = -32)
+    chunks = split_on_silence(song, min_silence_len = 1000, silence_thresh = -45)
     try:
         os.mkdir(path+'_audio_chunks')
     except(FileExistsError):
@@ -36,7 +27,7 @@ def silencebasedconversion(path: str):
         chunksilent = AudioSegment.silent(duration = 100)
         audio_chunk = chunksilent + ck+ chunksilent
         print("saving chunk{0}.wav".format(i))
-        audio_chunk.export("./chunk{0}.wav".format(i), bitrate ='192k', format ="wav", )
+        audio_chunk.export("./chunk{0}.wav".format(i), bitrate ='192k', format ="wav" )
         filename = 'chunk'+str(i)+'.wav'
         print("Processing chunk "+str(i))
         file = filename
@@ -51,11 +42,13 @@ def silencebasedconversion(path: str):
         except sr.RequestError as e:
             print("Could not request results. check the internet connection")
         i += 1
-        os.chdir('..')
+    os.chdir('..')
 
 
+getVideoToWav("temoignage.mp4") ## Export video to wav audio
 
 list_of_files = sorted( filter( lambda x: os.path.isfile(os.path.join("audio_chunks", x)), os.listdir("audio_chunks") ) )
 os.chdir("audio_chunks")
-##for f in list_of_files:
-  ##  silencebasedconversion(f)
+for f in list_of_files:
+    if f.endswith('.wav'):
+        silencebasedconversion(f)
